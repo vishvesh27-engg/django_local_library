45c29d1bab52841309eabd04787e38734d3d44af
vishvesh27-engg <vishveshwarrier@gmail.com>
2021-08-31 11:50:08 +0530

    renamed the app in heroku

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-ALLOWED_HOSTS = ['pure-dusk-69004.herokuapp.com',]
+ALLOWED_HOSTS = ['vswlibrary.herokuapp.com',]
 
 
 # Application definition
